1ebee998af1a0a45269443a16c397aa8a235b036
Will Zhang <610341647@qq.com>
2017-08-03 20:25:36 +0800

    添加mongodb 记录聊天记录

--- server.js
+++ server.js
@@ -3,14 +3,27 @@ const
 	url  = require('url'),
 	path = require('path'),
 	http = require('http'),
+	mongoClient = require('mongodb').MongoClient, // Mongo 客户端
+	dbConnect = 'mongodb://localhost:27017/chat', // 数据库连接
 	hostname = '127.0.0.1',
 	port = 3000;
 
-	var defaultPage = ['index.html'];
+var defaultPage = ['index.html'];
 
-	var fsroot = path.join(path.resolve(process.argv[2] || '.'),'/www');
+var fsroot = path.join(path.resolve(process.argv[2] || '.'), '/www');
 
-	const server = http.createServer((request, response) => {
+var insertData = function(db, data, callback) {
+	var collection = db.collection('chat');
+	collection.insert(data, function(err, result) {
+		if (err) {
+			console.log('Error :' + err);
+			return;
+		}
+		callback(result);
+	});
+}
+
+const server = http.createServer((request, response) => {
 	//获得URL的path
     var pathname = url.parse(request.url).pathname;
 
@@ -60,7 +73,7 @@ io.on('connection', (socket) => {
 			usocket[username] = socket;
 			user.push(username);
 			socket.emit('login',user);
-			socket.broadcast.emit('user joined',username,(user.length-1));
+			socket.broadcast.emit('user joined', username, (user.length - 1));
 			console.log(user);
 		}
 	})
@@ -70,6 +83,13 @@ io.on('connection', (socket) => {
 		if(res.recipient in usocket) {
 			usocket[res.recipient].emit('receive private message', res);
 		}
+		mongoClient.connect(dbConnect, res, function(err, db) {
+			console.log('链接成功');
+			insertData(db, res, function(result) {
+				// console.log(result);
+				db.close();
+			})
+		})
 	});
 
 	socket.on('disconnect', function(){
@@ -79,7 +99,7 @@ io.on('connection', (socket) => {
 			user.splice(user.indexOf(socket.username), 1);
 		}
 		console.log(user);
-		socket.broadcast.emit('user left',socket.username)
+		socket.broadcast.emit('user left', socket.username)
 	})
 
 });
