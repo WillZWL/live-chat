f891d951b6b9569c37237f36aaf88c1f1bccd7fe
Will Zhang <610341647@qq.com>
2017-08-05 12:21:38 +0800

    使用thinkjs2.0  重构

---
+++ src/home/controller/index.js
@@ -0,0 +1,103 @@
+'use strict';
+
+import Base from './base.js';
+
+var usernames = [];
+var usocket = {};
+var numUsers = 0;
+
+export default class extends Base {
+  // 客服登录页面
+  userloginAction() {
+    let id = this.cookie('user_id');
+    if (id) {
+      this.http.redirect('/home/index/index');
+    }
+    return this.display();
+  }
+
+  // 登录
+  async loginAction() {
+    let username = this.post('username');
+    let password = this.post('password');
+    let user = await this.model('user').where({name: username, password: password}).find();
+    if (user.id !== undefined && user.id > 0) {
+      let id = this.cookie('user_id', user.id);
+      return this.success(id);
+    } else {
+      return this.fail('login fail');
+    }
+  }
+
+  // 判断是否已登录
+  indexAction(){
+    let id = this.cookie('user_id');
+    if (id) {
+      return this.display();
+    } else {
+      this.http.redirect('/home/index/userlogin');
+    }
+  }
+
+  // 开启会话
+  openAction(self){
+    var socket = self.http.socket;
+    var data = self.http.data;
+    this.broadcast('new message', {
+      username: socket.username,
+      message: self.http.data
+    });
+  }
+
+  // 添加用户
+  adduserAction(self){
+    var socket = self.http.socket;
+    var username = self.http.data;
+    if (username) {
+      if (!(username in usocket)) {
+        socket.username = username;
+        if (usernames.indexOf(username) === -1) {
+          usernames.push(username);
+          numUsers++;
+        }
+        usocket[username] = this.http.socket;
+        this.broadcast('userjoin', username, username.length - 1);
+      }
+      this.emit('login', usernames);
+    }
+  }
+
+  // 关闭
+  closeAction(self){
+    var socket = self.http.socket;
+    if (socket.username) {
+      delete usocket[socket.username];
+      delete usernames[socket.username]
+      this.broadcast('userleft', socket.username);
+    }
+  }
+  // 会话
+  chatAction(self){
+    var socket = self.http.socket;
+    var data = self.http.data;
+    console.log(data.recipient);
+    if (data.recipient in usocket) {
+      usocket[data.recipient].emit('receive private message', data);
+    }
+  }
+  // 正在输入
+  typingAction(self){
+    var socket = self.http.socket;
+    this.broadcast('typing', {
+      username: socket.username
+    });
+  }
+
+  // 停止输入
+  stoptypingAction(self){
+    var socket = self.http.socket;
+    this.broadcast('stoptyping', {
+      username: socket.username
+    });
+  }
+}
